742881fcf73f9ded9cb06003c023a1524e30b93a
David Albuquerque <davidra_rj@hotmail.com>
2020-11-15 01:15:50 -0300

    +finished simple rsa

--- rsa.py
+++ rsa.py
@@ -1,8 +1,80 @@
-
 import random
 import time
 
+#==================Dicionario==================
+códigos_para_símbolos = {111: '0', 112: '1', 113: '2', 114: '3', 115: '4',
+116: '5', 117: '6', 118: '7', 119: '8', 121: '9', 122: '=', 123: '+',
+124: '-', 125: '/', 126: '*', 127: 'a', 128: 'b', 129: 'c', 131: 'd',
+132: 'e', 133: 'f', 134: 'g', 135: 'h', 136: 'i', 137: 'j', 138: 'k',
+139: 'l', 141: 'm', 142: 'n', 143: 'o', 144: 'p', 145: 'q', 146: 'r',
+147: 's', 148: 't', 149: 'u', 151: 'v', 152: 'w', 153: 'x', 154: 'y',
+155: 'z', 156: 'á', 157: 'à', 158: 'â', 159: 'ã', 161: 'é', 162: 'ê',
+163: 'í', 164: 'ó', 165: 'ô', 166: 'õ', 167: 'ú', 168: 'ç', 169: 'A',
+171: 'B', 172: 'C', 173: 'D', 174: 'E', 175: 'F', 176: 'G', 177: 'H',
+178: 'I', 179: 'J', 181: 'K', 182: 'L', 183: 'M', 184: 'N', 185: 'O',
+186: 'P', 187: 'Q', 188: 'R', 189: 'S', 191: 'T', 192: 'U', 193: 'V',
+194: 'W', 195: 'X', 196: 'Y', 197: 'Z', 198: 'Á', 199: 'À', 211: 'Â',
+212: 'Ã', 213: 'É', 214: 'Ê', 215: 'Í', 216: 'Ó', 217: 'Ô', 218: 'Õ',
+219: 'Ú', 221: 'Ç', 222: ',', 223: '.', 224: '!', 225: '?', 226: ';',
+227: ':', 228: '_', 229: '(', 231: ')', 232: '"', 233: '#', 234: '$',
+235: '%', 236: '@', 237: ' ', 238: '\n'}
+
+símbolos_para_códigos = {'0': 111, '1': 112, '2': 113, '3': 114, '4': 115,
+'5': 116, '6': 117, '7': 118, '8': 119, '9': 121, '=': 122, '+': 123,
+'-': 124, '/': 125, '*': 126, 'a': 127, 'b': 128, 'c': 129, 'd': 131,
+'e': 132, 'f': 133, 'g': 134, 'h': 135, 'i': 136, 'j': 137, 'k': 138,
+'l': 139, 'm': 141, 'n': 142, 'o': 143, 'p': 144, 'q': 145, 'r': 146,
+'s': 147, 't': 148, 'u': 149, 'v': 151, 'w': 152, 'x': 153, 'y': 154,
+'z': 155, 'á': 156, 'à': 157, 'â': 158, 'ã': 159, 'é': 161, 'ê': 162,
+'í': 163, 'ó': 164, 'ô': 165, 'õ': 166, 'ú': 167, 'ç': 168, 'A': 169,
+'B': 171, 'C': 172, 'D': 173, 'E': 174, 'F': 175, 'G': 176, 'H': 177,
+'I': 178, 'J': 179, 'K': 181, 'L': 182, 'M': 183, 'N': 184, 'O': 185,
+'P': 186, 'Q': 187, 'R': 188, 'S': 189, 'T': 191, 'U': 192, 'V': 193,
+'W': 194, 'X': 195, 'Y': 196, 'Z': 197, 'Á': 198, 'À': 199, 'Â': 211,
+'Ã': 212, 'É': 213, 'Ê': 214, 'Í': 215, 'Ó': 216, 'Ô': 217, 'Õ': 218,
+'Ú': 219, 'Ç': 221, ',': 222, '.': 223, '!': 224, '?': 225, ';': 226,
+':': 227, '_': 228, '(': 229, ')': 231, '"': 232, '#': 233, '$': 234,
+'%': 235, '@': 236, ' ': 237, '\n': 238}
+
+
 #==============Funcoes Principais==============
+def converte_texto(texto):
+  """Converte o texto usando a tabela de conversão."""
+  texto_convertido = str(texto)
+  for simbolo, codigo in símbolos_para_códigos.items():
+    texto_convertido = texto_convertido.replace(simbolo, str(codigo))
+
+  return texto_convertido
+
+def desconverte_texto(texto):
+  """Desconverte o texto convertido utilizando a função converte_texto()."""
+  texto_desconvertido = str(texto)
+  texto_desconvertido = texto_desconvertido.replace("[","")
+  texto_desconvertido = texto_desconvertido.replace("]","")
+  texto_desconvertido = texto_desconvertido.replace(",","")
+  texto_desconvertido = texto_desconvertido.replace(" ","")
+  texto_desconvertido = [texto_desconvertido[i:i+3] for i in range(0, len(texto_desconvertido), 3)]
+  for iterator, value in enumerate(texto_desconvertido):
+    for codigo, simbolo in códigos_para_símbolos.items():
+      if(str(value) == str(codigo)):
+        texto_desconvertido[iterator] = simbolo
+        break
+
+  texto_desconvertido = ''.join(texto_desconvertido)
+  
+  return texto_desconvertido
+
+def quebra_blocos(n,texto):
+  """Quebra uma cadeia de caracteres ou números em blocos de tamanho n."""
+  texto = str(texto)
+  texto_blocos = [texto[i:i+n] for i in range(0, len(texto), n)]
+  return texto_blocos
+
+def desquebra_blocos(texto_blocos):
+  """Junta os blocos resultante da quebra pela função quebra_blocos."""
+  texto = ''.join(str(texto_blocos))
+  return texto
+
 def gera_primos(n):
     """Funcao responsavel por gerar um numero primo de tamanho n atraves de tentativas pelo teste de Miller-Rabin."""
     ini = time.time()
@@ -23,7 +95,7 @@ def gera_primos(n):
             if(Miller_Rabin(primo,base) == "inconclusivo"):
                 i+=1
     fim = time.time()
-    print("============ A funcao gera_primos demorou: {} segundos ============".format(fim-ini))
+    #print("============ A funcao gera_primos demorou: {} segundos ============".format(fim-ini))
     return primo
 
 def gera_chaves(dp,dq):
@@ -53,42 +125,46 @@ def gera_chaves(dp,dq):
     d = euclides_estendido(e,phi)
     d += phi
 
-    print("P: {}".format(p))
-    print("Q: {}".format(q))
-    print("Chave publica N: {}".format(n))
-    print("Phi: {}".format(phi))
-    print("Chave pubilca E: {}".format(e))
-    print("Chave privada D: {}".format(d))
-    print("Numero de digitos de P: {}".format(len(str(p))))
-    print("Numero de digitos de Q: {}".format(len(str(q))))
-
-    n = p*q
+    #print("P: {}".format(p))
+    #print("Q: {}".format(q))
+    #print("Chave publica N: {}".format(n))
+    #print("Phi: {}".format(phi))
+    #print("Chave pubilca E: {}".format(e))
+    #print("Chave privada D: {}".format(d))
+    #print("Numero de digitos de P: {}".format(len(str(p))))
+    #print("Numero de digitos de Q: {}".format(len(str(q))))
+    #print("Numero de digitos de N: {}".format(len(str(n))))
 
     fim = time.time()
-    print("============ A funcao gera_chaves demorou: {} segundos ============".format(fim-ini))
+    #print("============ A funcao gera_chaves demorou: {} segundos ============".format(fim-ini))
 
     return n, e, d
     #return p,q
 
 def encriptar(texto,n,e):
-    """Funcao responsavel por encriptar uma string texto usando as chaves publicas n (modulo) e e (expoente)."""
+    """Funcao responsavel por encriptar uma string texto em blocos usando as chaves publicas n (modulo) e e (expoente)."""
     print("=================")
     print("Encriptando texto")
     print("=================")
-    texto_numero = texto
-    texto_encriptado = pow(texto_numero,e,n) 
-    print("Texto encriptado: {}".format(texto_encriptado))
-    return texto_encriptado
+    texto_numero = converte_texto(texto)
+    texto_numero_blocos = quebra_blocos(len(str(n))-1,texto_numero)
+    for index, value in enumerate(texto_numero_blocos):
+      texto_numero_blocos[index] = pow(int(value),e,n) 
+    #print("Texto encriptado em blocos: {}".format(texto_numero_blocos))
+    return texto_numero_blocos
 
 
 def descriptar(texto, n, d):
-    """Funcao responsavel por descriptar uma string texto usando as chaves publicas n (modulo) e e (expoente)."""
+    """Funcao responsavel por descriptar blocos de string encriptadas usando as chaves publicas n (modulo) e e (expoente)."""
     print("==================")
     print("Descriptando texto")
     print("==================")
-    texto_numero = texto
-    texto_descriptado = pow(texto_numero, d, n)
-    print("Texto descriptado: {}".format(texto_descriptado))
+    for index, value in enumerate(texto):
+      texto[index] = pow(int(value), d, n)
+    texto_desquebrado = desquebra_blocos(texto)
+    #print("Texto desquebrado: {}".format(texto_desquebrado))
+    texto_descriptado = desconverte_texto(texto_desquebrado)
+    #print("Texto descriptado: {}".format(texto_descriptado))
     return texto_descriptado
 
 #==============Algoritmos Auxiliares==============
@@ -151,13 +227,15 @@ def euclides_estendido(a, b):
         #     resto, ", x_ant: ", x_ant, ", y_ant: ", y_ant, ", x_novo: ", x_novo, ", y_novo: ", y_novo)
     return x_ant
 
-
 #Main
 
 print("=========================RSA=========================")
 ini = time.time()
 n, e, d = gera_chaves(50, 100)
-descriptar(encriptar(123456789, n, e), n, d)
+print(descriptar(encriptar("Hi! Im a simple message!", n, e), n, d))
 fim = time.time()
 print("O processo completo levou: {}".format(fim-ini))
 print("=========================RSA=========================")
+
+#print(quebra_blocos(2,1234567))
+#print(desquebra_blocos(quebra_blocos(2, 1234567)))
